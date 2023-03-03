b6f265e2b0226aa1abd70e267c65c620d2af97f4
Miguel Sanabria <miguel.sanabria@uao.edu.co>
2023-03-03 17:18:54 -0500

    Update fishcolors & gtk4.0

---
+++ hypr/scripts/fishcolors.fish
@@ -0,0 +1,12 @@
+#!/bin/fish
+
+set -U fish_color_operator d3869b
+set -U fish_color_escape d3869b
+set -U fish_color_autosuggestion 83a598
+set -U fish_color_comment default
+set -U fish_color_param 458588
+set -U fish_color_error fb4934
+set -U fish_color_end fe8019
+set -U fish_color_redirection fe8019
+set -U fish_color_quote fabd2f
+set -U fish_color_command 689d6a
